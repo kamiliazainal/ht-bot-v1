30b0ac6aa5b486583c0119b651f3f234a5cae691
kamiliazainal <47439061+kamiliazainal@users.noreply.github.com>
2019-04-23 10:38:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "EAAFBIx0k8OUBAOt0Vut47XcUBXWaf064ZAR8ZBSSHssubEp8faIfntWtISi9TEVGQraXXgGmFxK1QZB2fArH5vHuQbGM9lCQDqw88R5ZASW121hYaxTZCLK5nTuzjaxHGOIsiY8GHTCTIedyhSsZBik076a2Bewaowv1ZAsBRceLQZDZD": PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
